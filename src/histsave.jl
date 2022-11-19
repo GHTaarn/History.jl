d665d92f4b816776cfcebc5e6cf1d9880b1559fe
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2022-11-19 19:26:07 +0100

    Option to specify precise list of output items for histsave()

--- src/histsave.jl
+++ src/histsave.jl
@@ -1,16 +1,18 @@
 """
-    histsave(fname::AbstractString, nitems::Union{Symbol,Integer}=:all)
+    histsave(fname::AbstractString, indices::Union{Symbol,AbstractVector{<:Integer}}=:all)
+    histsave(fname::AbstractString, nitems::Integer)
 
 Save the REPL history in a file named `fname`. You can optionally restrict
-the output to the `nitems` most recent items in the REPL history.
+the output to the `nitems` most recent items in the REPL history or specify
+`indices` as the specific indices of the items to be output to the file.
 
 See also [`history`](@ref).
 """
-function histsave(fname::AbstractString, nitems::Union{Symbol,Integer}=:all)
-    @assert nitems == :all || nitems >= 0
+function histsave(fname::AbstractString, itemspec::Union{Symbol,Integer,AbstractVector{<:Integer}}=:all)
+    @assert itemspec == :all || itemspec isa AbstractVector{<:Integer} || itemspec >= 0
     n = 0
     open(fname, "w") do f
-        foreach(history()[(nitems == :all ? 1 : end+1-nitems):end,3]) do y
+        foreach(history()[(itemspec isa AbstractVector ? itemspec : ((itemspec == :all ? 1 : end+1-itemspec):end)),3]) do y
             n += write(f, y*"\n")
         end
     end
