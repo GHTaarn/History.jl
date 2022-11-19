010f797598546a8f658a7eae460b473a8ee334e2
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2022-11-19 18:37:02 +0100

    Option to restrict histsave output

--- src/histsave.jl
+++ src/histsave.jl
@@ -1,14 +1,16 @@
 """
-    histsave(fname::AbstractString)
+    histsave(fname::AbstractString, nitems::Union{Symbol,Integer}=:all)
 
-Save the REPL history in a file named `fname`.
+Save the REPL history in a file named `fname`. You can optionally restrict
+the output to the `nitems` most recent items in the REPL history.
 
 See also [`history`](@ref).
 """
-function histsave(fname::AbstractString)
+function histsave(fname::AbstractString, nitems::Union{Symbol,Integer}=:all)
+    @assert nitems == :all || nitems >= 0
     n = 0
     open(fname, "w") do f
-        foreach(history()[:,3]) do y
+        foreach(history()[(nitems == :all ? 1 : end+1-nitems):end,3]) do y
             n += write(f, y*"\n")
         end
     end
