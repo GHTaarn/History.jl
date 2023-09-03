934375885f18246f518491204ad2b6bca65fe526
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2023-09-03 09:24:48 +0200

    Julia 1.0 compatibiity fixes

--- test/histsearch.jl
+++ test/histsearch.jl
@@ -6,7 +6,7 @@ using History
     h = string.('a':'z')
     History.redefine_basehist!(() -> (; modes=modes, history=h))
 
-    @test histsearch(>("x")) == [25 :y "y"; 26 :z "z"]
+    @test histsearch(s -> s > "x") == [25 :y "y"; 26 :z "z"]
     @test histsearch("b") == [2 :b "b"]
     @test histsearch(r"^[ce]") == [3 :c "c"; 5 :e "e"]
     @test size(histsearch(r"^ce"), 1) == 0
