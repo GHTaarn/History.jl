e96383b6db5ab7e6a18b77cfde311ff17ae8a055
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2025-12-06 14:09:40 +0100

    Improved histsave()
    The "fname" argument is now optional
    Error instead of clobbering existing output files
    Option to save only the current REPL session

--- src/histsave.jl
+++ src/histsave.jl
@@ -1,22 +1,63 @@
-"""
-    histsave(fname::AbstractString, indices::Union{Symbol,AbstractVector{<:Integer}}=:all)
-    histsave(fname::AbstractString, nitems::Integer)
+using Dates: now
 
-Save the REPL history in a file named `fname`. You can optionally restrict
-the output to the `nitems` most recent items in the REPL history (including the
-current line) or specify
-`indices` as the specific indices of the items to be output to the file.
+"""
+    histsave(fname::AbstractString, itemspec::Symbol=:all; sessionseparator=row->contains(row[3],"exit()"))
+    histsave(fname::AbstractString, nitems::Integer)
+    histsave(fname::AbstractString, indices::AbstractVector{<:Integer})
+
+Save the REPL history in a file named `fname`.
+
+In the first variant, `itemspec` may be either `:all` or `:session`.
+In the latter case `histsave` attempts to determine what part of the history
+belongs to the current Julia session and saves this, but it is advisable to
+check the output as it is heuristically determined using the `sessionseparator`
+keyword and deviations are especially common if several Julia sessions have
+been running concurrently with the current session.
+
+The second variant saves only the `nitems` most recent items in the REPL history
+(including the current line).
+
+In the third variant `indices` is the specific indices of the items to be saved.
+
+In all variants, an error is thrown if the file `fname` exists and is non-empty.
+Also, `fname` may be omitted, in which case the history will be written to the
+file `"\$(Dates.now()).jl"`.
 
 See also [`history`](@ref).
 """
-function histsave(fname::AbstractString, itemspec::Union{Symbol,Integer,AbstractVector{<:Integer}}=:all)
-    @assert itemspec == :all || itemspec isa AbstractVector{<:Integer} || itemspec >= 0
+function histsave(fname::AbstractString, itemspec::Union{Symbol,AbstractVector{<:Integer},Integer}; sessionseparator=row->contains(row[3],"exit()"))
+    @assert itemspec in (:all, :session) || itemspec isa AbstractVector{<:Integer} || itemspec >= 0
     n = 0
-    open(fname, "w") do f
-        foreach(history()[(itemspec isa AbstractVector ? itemspec : ((itemspec == :all ? 1 : end+1-itemspec):end)),3]) do str
+    open(fname, "a+") do f
+        seek(f, 0)
+        length(read(f, 1)) > 0 && error("Refusing to overwrite non-empty file \"$fname\"")
+        histry = history()
+        lhistry = size(histry, 1)
+        idxs = if itemspec isa AbstractVector
+            itemspec
+        elseif itemspec == :all
+            1:lhistry
+        elseif itemspec isa Integer
+            lhistry+1-itemspec:lhistry
+        elseif itemspec == :session
+            idx = findlast(sessionseparator, eachrow(histry))
+            if isnothing(idx) || idx == lhistry
+                @warn "No usable match for sessionseparator was found, so all history will be saved"
+                idx = 1
+            else
+                idx += 1
+            end
+            idx:lhistry
+        end
+        foreach(histry[idxs,3]) do str
             n += write(f, str * "\n")
         end
     end
     "$n bytes written to $fname"
+end
+
+function histsave(itemspec::Union{Symbol,AbstractVector{<:Integer},Integer}; kwargs...)
+    fname = "$(now()).jl"
+    histsave(fname, itemspec; kwargs...)
 end
 
